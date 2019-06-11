06682800d4e2f35d3ab6d509042f50d05e0ae40f
PramanSatya <pramansatya@gmail.com>
2019-06-11 13:35:20 +0530

    correction of indentation in ch-6 Q-1

--- javascript/ch_06/Q_1.js
+++ javascript/ch_06/Q_1.js
@@ -1,16 +1,16 @@
 class Vec{
   constructor(x, y){
-  this.x = x;
-  this.y = y;
+    this.x = x;
+    this.y = y;
   }
   plus(vec2){
-  return new Vec(this.x + vec2.x, this.y + vec2.y);
+    return new Vec(this.x + vec2.x, this.y + vec2.y);
   }
   minus(vec2){
-  return new Vec(this.x - vec2.x, this.y - vec2.y);
+    return new Vec(this.x - vec2.x, this.y - vec2.y);
   }
   get length(){
-  return Math.sqrt(this.x * this.x + this.y * this.y);
+    return Math.sqrt(this.x * this.x + this.y * this.y);
   }
 }
 console.log(new Vec(1, 2).plus(new Vec(2, 3)));
